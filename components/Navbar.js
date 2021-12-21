5779ddbb8ef6603ce127db5cce0b5a702963e14f
axolotlbyte <arjaankhan@gmail.com>
2021-12-21 22:56:19 +0530

    sidebar, navlinks

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react"
 import logo from "../public/images/saph-logo.webp"
+import Sidebar from "./Sidebar"
 
 export default function Navbar() {
+
+    const [active, setActive] = useState(false)
+
+    const handleActive = () => setActive(!active)
+    
     return (
-        <nav id="navbar" className="fixed flex z-50 justify-between w-full items-center h-22 bg-tranparent">
+        <>
+        <nav id="navbar" className="fixed flex z-40 justify-between w-full items-center h-22 bg-tranparent">
             <div className="p-3 cursor-pointer">
                 <Image 
                     src={"/images/saph-logo.webp"}
@@ -13,13 +22,40 @@ export default function Navbar() {
                     height={70}
                 />
             </div>
-            <div>
-                <button className="md:hidden px-4">
+            <div className="mr-4">
+                <button className={"md:hidden transition-all" + ( active ? " hidden" : "")} onClick={handleActive}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </button>
+                <ul onClick={handleActive} className="w-full hidden lg:flex justify-center z-50 pl-4 cursor-pointer">
+                    <li className="w-full flex justify-center text-xl p-1 px-6  border-collapse bg-white rounded-r-2xl rounded-l-2xl text-black font-semibold hover:border-none ">
+                        <Link className="w-full" href="/">
+                            <a>SHOP</a>
+                        </Link>
+                    </li>
+                    <li className="w-full text-xl p-1 px-6  border-collapse text-white font-semibold hover:border-none ">
+                        <Link className="w-full" href="/">
+                            <a>Blog</a>
+                        </Link>
+                    </li>
+                    <li className="w-full text-xl p-1 px-6  border-collapse text-white font-semibold hover:border-none ">
+                        <Link className="w-full" href="/">
+                            <a>About</a>
+                        </Link>
+                    </li>
+                    <li className="w-full text-xl p-1 px-6  border-collapse text-white font-semibold hover:border-none ">
+                        <Link className="w-full" href="/">
+                            <a>Contact</a>
+                        </Link>
+                    </li>
+            </ul>
             </div>
         </nav>
+        <div className="flex">
+            <div onClick={handleActive} className={"fixed lg:hidden h-screen bg-black bg-opacity-30 z-50 w-full" + (active ? "" : " hidden")}></div>
+            <Sidebar handleActive={handleActive} active={active}/>
+        </div>
+        </>
     )
 }

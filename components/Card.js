aae9febfeb6500335a4a16de5d99fc0431d98c47
axolotlbyte <arjaankhan@gmail.com>
2022-01-14 15:00:43 +0530

    minor

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,11 @@
 export default function Card(props) {
   return (
     <>
-      <div className="relative overflow-hidden rounded-lg card bg-slate-100 h-card">
-        <button className="absolute flex items-center justify-center text-white h-14 w-14 bg-slate-900 right-0 rounded-bl-lg ">
+      <div className="relative bg-red-800 overflow-hidden rounded-3xl card h-card">
+        <button className="absolute flex items-center justify-center text-white h-14 w-14 bg-slate-900 right-2 top-2 rounded-full ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 right-1 bottom-1 absolute"
+            className="h-4 w-4 right-2 bottom-2 absolute"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -19,7 +19,7 @@ export default function Card(props) {
           </svg>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10 absolute"
+            className="h-9 w-9 absolute"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -33,16 +33,17 @@ export default function Card(props) {
           </svg>
         </button>
         <div className="w-full h-full"></div>
-        <div id="prod-name" className="w-full h-16 bg-slate-900 bottom-0 absolute">
-            <p className="text-white text-3xl">Product Name</p>
-            <p className="text-white text-lg">$33.55</p>
+
+        <div id="prod-name" className="w-full bg-transparent h-auto bg-slate-900 bottom-0 absolute">
+        <p className="text-white text-5xl lg:text-4xl text-left px-4">Product Name</p> 
+        <p className="text-white text-2xl font-light text-right px-6 pb-2">$33.55</p>
         </div>
       </div>
       <style jsx>{`
         .card {
-          aspect-ratio: 1 / 1.195;
+          aspect-ratio: 1 / 1;
         }
-        .card:hover #prod-name,.card:hover button {
+        .card:hover.card:hover button {
           background-color: #e3176c;
         }
         button:hover svg{

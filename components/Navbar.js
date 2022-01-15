abb00947f48aa710f49823b3af39556765f00587
axolotlbyte <arjaankhan@gmail.com>
2022-01-15 13:42:36 +0530

    major

--- components/Navbar.js
+++ components/Navbar.js
@@ -69,34 +69,9 @@ export default function Navbar({ home }) {
           </svg>
         </div>
         <div className="mr-4 flex items-center">
-          <ul
-            onClick={handleActive}
-            className="w-full hidden lg:flex justify-center items-start z-50 pl-4 cursor-pointer"
-          >
-            <li className="w-full flex justify-center mx-2 text-xl hover:bg-transparent hover:text-white p-1 px-8 border-2 border-white border-collapse bg-white rounded-r-3xl rounded-l-3xl text-black font-semibold ">
-              <Link className="w-full" href="/shop">
-                <a>SHOP</a>
-              </Link>
-            </li>
-            <li className="w-full text-xl p-1 px-6 mx-2 hover:bg-slate-400 border-collapse text-white font-semibold hover:border-none rounded-r-3xl rounded-l-3xl">
-              <Link className="w-full" href="/blog">
-                <a>Blog</a>
-              </Link>
-            </li>
-            <li className="w-full text-xl p-1 px-6 mx-2 hover:bg-slate-400 border-collapse text-white font-semibold hover:border-none rounded-r-3xl rounded-l-3xl">
-              <Link className="w-full" href="/about">
-                <a>About</a>
-              </Link>
-            </li>
-            <li className="w-full text-xl p-1 px-6 mx-2 hover:bg-slate-400 border-collapse text-white font-semibold hover:border-none rounded-r-3xl rounded-l-3xl">
-              <Link className="w-full" href="/contact">
-                <a>Contact</a>
-              </Link>
-            </li>
-          </ul>
           <button
             className={
-              "transition-all w-10 flex items-center justify-center p-1.5 hover:text-black rounded-full hover:bg-slate-300 text-white h-10"
+              "transition-all w-10 lg:mx-1 flex items-center justify-center p-1.5 hover:text-black rounded-full hover:bg-slate-300 text-white h-10"
             }
             title="wishlist"
           >
@@ -129,6 +104,13 @@ export default function Navbar({ home }) {
             >
               <path d="M8 1a2 2 0 0 1 2 2v2H6V3a2 2 0 0 1 2-2zm3 4V3a3 3 0 1 0-6 0v2H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11zm-1 1v1.5a.5.5 0 0 0 1 0V6h1.639a.5.5 0 0 1 .494.426l1.028 6.851A1.5 1.5 0 0 1 12.678 15H3.322a1.5 1.5 0 0 1-1.483-1.723l1.028-6.851A.5.5 0 0 1 3.36 6H5v1.5a.5.5 0 1 0 1 0V6h4z" />
             </svg>
+          </button>
+          <button
+            className={
+              "transition-all w-10 mr-2 lg:mx-2 flex items-center justify-center p-1.5 rounded-full bg-gradient-to-tl from-cyan-500 via-red-500 to-sky-600 h-10 border-2 border-white"
+            }
+            title="user"
+          >
           </button>
           <button
             className={"md:hidden transition-all" + (active ? " hidden" : "")}
